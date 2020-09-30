cf92c3f57fea049fcb9f3e8570ad57ca18ceb5be
jhankarpHero <61257321+jhankarpHero@users.noreply.github.com>
2020-09-29 19:26:38 -0500

    root api created

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get('/', (req, res) =>{
+    res.send("hello from db it's working working")
+})
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
